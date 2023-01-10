78800c6ceaaa9c1ca2e359a3dcb474feeb34eb99
Kiran9991 <122242670+Kiran9991@users.noreply.github.com>
2023-01-10 20:19:39 +0530

    task 2 getelementbyid

--- demo.js
+++ demo.js
@@ -1,1 +1,7 @@
-console.log('hello world');
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+var title = document.getElementById('title1');
+title.style.color = 'green';
+title.style.fontWeight = 'bold';
+
+ 
