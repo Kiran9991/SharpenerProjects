05c4ecbf07b5f0bd9b964b4057f18d8f50409fd1
Kiran9991 <122242670+Kiran9991@users.noreply.github.com>
2023-01-11 19:26:25 +0530

    task 5 - GetElementsbyTAgName

--- demo.js
+++ demo.js
@@ -3,13 +3,19 @@
 // var title = document.getElementById('title1');
 // title.style.color = 'green';
 // title.style.fontWeight = 'bold';
-var items = document.getElementsByClassName('list-group-item');
 // console.log(items);
 // console.log(items[1]);
 // items[1].textContent = 'Kiran';
 // items[1].style.fontWeight = 'bold'
-items[2].style.color = 'green';
-for(var i=0; i<items.length; i++) {
-    items[i].style.fontWeight = 'bold';
-} 
+// var items = document.getElementsByClassName('list-group-item');
+// items[2].style.color = 'orange';
+// for(var i=0; i<items.length; i++) {
+//     li[i].style.backgroundColor = 'yellow';
+// }
+var li = document.getElementsByTagName('li');
+li[2].style.color = 'green';
+for(var i=0; i<li.length; i++) {
+    li[i].style.fontWeight = 'bold';
+    li[i].style.backgroundColor = 'yellow';
+}
  
