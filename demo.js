6a9a9347f2977e1710e9ac4e9c1b56b49279a6fd
Kiran9991 <122242670+Kiran9991@users.noreply.github.com>
2023-01-11 18:44:15 +0530

    Task 4 - GetElementsbyClassname

--- demo.js
+++ demo.js
@@ -1,7 +1,15 @@
-var header = document.getElementById('main-header');
-header.style.borderBottom = 'solid 3px #000';
-var title = document.getElementById('title1');
-title.style.color = 'green';
-title.style.fontWeight = 'bold';
-
+// var header = document.getElementById('main-header');
+// header.style.borderBottom = 'solid 3px #000';
+// var title = document.getElementById('title1');
+// title.style.color = 'green';
+// title.style.fontWeight = 'bold';
+var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Kiran';
+// items[1].style.fontWeight = 'bold'
+items[2].style.color = 'green';
+for(var i=0; i<items.length; i++) {
+    items[i].style.fontWeight = 'bold';
+} 
  
