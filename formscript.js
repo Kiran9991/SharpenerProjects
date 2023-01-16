60b4f01a23cfd20180147dd3317b61e75e375994
kiran <kiranagiwale3@gmail.com>
2023-01-16 11:54:24 +0530

    Lets store the users in the right format

--- formscript.js
+++ formscript.js
@@ -1,15 +1,30 @@
 //  USER FORM SCRIPT
 
 // Put DOM elements into variables
-const myForm = document.querySelector('#my-form');
-const nameInput = document.querySelector('#name');
-const emailInput = document.querySelector('#email');
+// const myForm = document.querySelector('#my-form');
+// const nameInput = document.querySelector('#name');
+// const emailInput = document.querySelector('#email');
 
 // Listen for form submit
-myForm.addEventListener('submit', onSubmit);
+// myForm.addEventListener('submit', onSubmit);
 
-function onSubmit(e) {
-  e.preventDefault();
-  localStorage.setItem('username', nameInput.value);
-  localStorage.setItem('useremail', emailInput.value);
+// function onSubmit(e) {
+//   e.preventDefault();
+//   localStorage.setItem('username', nameInput.value);
+//   localStorage.setItem('useremail', emailInput.value);
+// }
+
+let myObj = {
+    name: 'kiran',
+    email: 'kiran3@gmail.com'
 }
+
+let myObj_serialized = JSON.stringify(myObj);
+
+localStorage.setItem('myobj', myObj_serialized);
+
+console.log(myObj_serialized);
+
+let myOjb_deserialized = JSON.parse(localStorage.getItem('myobj'));
+
+console.log(myOjb_deserialized);
